81c01b4e645af2e8a930dc44b27232a30f8ca39c
moofazzel <mofazzel444@gmail.com>
2024-05-17 07:26:24 +0600

    changes in the dashboard home page

--- app/(dashboard)/dashboard/page.js
+++ app/(dashboard)/dashboard/page.js
@@ -1,29 +1,28 @@
 import AddReviewForm from "@/components/Dashboard/AddReviewForm";
-import SingleReview from "@/components/Dashboard/SingleReview";
 import { getAllReviews } from "@/database/queries";
 
 const DashboardHomePage = async () => {
   const reviews = await getAllReviews();
-  const allReviews = reviews[0]?.googleReviews;
+  // const allReviews = reviews[0]?.googleReviews;
 
   return (
     <section className="text-white text-2xl text-center ">
       <AddReviewForm />
 
-      <div>
+      {/* <div>
         <h1 className="font-bold mb-20">
-          {allReviews.length > 0
+          {allReviews?.length > 0
             ? "All Google Reviews"
             : "No Google Reviews Please Add "}
         </h1>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-          {allReviews.map((review) => {
+          {allReviews?.map((review) => {
             const reviewStr = JSON.stringify(review);
             return <SingleReview key={review._id} reviewStr={reviewStr} />;
           })}
         </div>
-      </div>
+      </div> */}
     </section>
   );
 };
